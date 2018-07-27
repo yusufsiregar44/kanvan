7292208d564813c87b2ba71fce71fab8b27f42c9
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-27 07:32:04 +0700

    finished ContentTile Component

--- client/src/components/BacklogColumn.vue
+++ client/src/components/BacklogColumn.vue
@@ -5,14 +5,22 @@
         <p>Backlog</p>
       </div>
       <div class="message-body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a>            efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+        <div class="tile is-ancestor">
+          <div class="tile is-vertical is-parent">
+            <ContentTile></ContentTile>
+          </div>
+        </div>
       </div>
     </article>
   </div>
 </template>
 
 <script>
+import ContentTile from '@/components/ContentTile.vue'
 export default {
+  components: {
+    ContentTile,
+  }
 }
 </script>
 
---
+++ client/src/components/ContentTile.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="tile is-child box has-text-centered">
+    <p class="title">Hello World</p>
+    <a class="button is-pulled-left" style="margin-left: 40px"><i class="fas fa-arrow-left"></i></a>
+    <a class="button" style="margin-right: 40px"><i class="fas fa-arrow-right"></i></a>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
